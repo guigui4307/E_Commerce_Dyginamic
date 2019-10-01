a0d4915e63248dca601659713bc4a23a8b620846
Guillaume <petit.guillaume43@gmail.com>
2019-10-01 15:41:36 +0200

    Affichage Article

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,12 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-
+import Article from'../Models/Article';
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css']
 })
 export class ListComponent implements OnInit {
-
+  listArticle:Article[]=[
+    new Article("article1","des1",5,20),
+    new Article("article2","des2",4,40),
+    new Article("article3","des3",3,60),
+    new Article("article4","des4",2,80),
+    new Article("article5","des5",1,100),
+  ];
   constructor() { }
 
   ngOnInit() {
