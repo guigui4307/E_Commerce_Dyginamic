47095a15f5ca0276e90753ae3ff644e1c19a3dc7
Guillaume <petit.guillaume43@gmail.com>
2019-10-01 15:09:08 +0200

    Add Classe User and Article

---
+++ src/app/Models/Article.ts
@@ -0,0 +1,12 @@
+class Article {
+    name:string;
+    descritpion:string;
+    quantity:number;
+    price:number;
+    constructor(nom:string, des:string, quant:number, prix:number) {
+        this.name=nom;
+        this.descritpion=des;
+        this.quantity=quant;
+        this.price=prix;
+    }
+}
---
+++ src/app/Models/User.ts
@@ -0,0 +1,10 @@
+class  Users {
+    name:string;
+    email:string;
+    passeword:string;
+    constructor(nom:string, e:string, pwd:string) {
+        this.name=nom;
+        this.email=e;
+        this.passeword=pwd;
+    }
+}
